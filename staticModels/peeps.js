d2e599a4b94e3203bfd73e73b0b9da41d1839b82
Talin Crow <crowtech7@gmail.com>
2020-08-13 14:59:29 -0500

    Completed basic frontend intergration with static data

---
+++ staticModels/peeps.js
@@ -0,0 +1,20 @@
+module.exports.peeps = [
+    {
+        id: 1,
+        username: 'talincrow',
+        firstname: 'Talin',
+        lastname: 'Crow'
+    },
+    {
+        id: 2,
+        username: 'underhill',
+        firstname: 'Frodo',
+        lastname: 'Baggins'
+    },
+    {
+        id: 3,
+        username: 'garderner',
+        firstname: 'Samwise',
+        lastname: 'Gamgee'
+    },
+];
